78d7227619a292203bda2d9c486c1a68893d3a0e
Domokos_Csaba <csabadomokos91@gmail.com>
2021-12-01 19:43:15 +0100

    js refreshed

--- Domokos_Csaba/hazifeladatDCS/gyakjs.js
+++ Domokos_Csaba/hazifeladatDCS/gyakjs.js
@@ -1,7 +1,36 @@
 
 
-function random(min, max) {
+/*function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
+function d6() {
+    return random(1, 6);
+}
 
+function returnRandomDiceThrow() {
+    let results = [0, 0, 0, 0, 0, 0];
+    return results;
+}
+
+console.log(results);
+
+
+
+function kockadobasszimulator () {
+    for( let i = 0; i < 1000; i++){
+    let kockadobas = d6();
+}
+} 
+
+kockadobasszimulator()
+*/
+
+let tomb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for (let i = 0; i < tomb.length; i++) {
+    sum += tomb[i];
+}
+
+console.log(sum);
