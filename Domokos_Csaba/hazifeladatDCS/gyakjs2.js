0b62ecc5a80195d46b7e403909ed3b11474815d1
Domokos_Csaba <csabadomokos91@gmail.com>
2021-12-01 19:57:04 +0100

    Osszegzes 2

---
+++ Domokos_Csaba/hazifeladatDCS/gyakjs2.js
@@ -0,0 +1,15 @@
+//Osszegzes2
+
+let tomb1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function sum2(tomb1) {
+    let sum = 0;
+    for (let i = 0; i < tomb.length; i++) {
+        if(tomb1[i] < 0){
+            sum++
+        }
+    }
+    return sum;
+}
+
+console.log(sum);
