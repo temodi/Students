56c255da0d4cbf1f50a64bb5553be4bad6c3d899
Pista_Laszlo <pistalaszlo@ymail.com>
2021-12-02 17:30:49 +0000

    lotto

--- Pista_Laszlo/JS/dec_1.js
+++ Pista_Laszlo/JS/dec_1.js
@@ -1,4 +1,4 @@
-function rollDie(min, max) {
+/* function rollDie(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -73,4 +73,44 @@ function majdMostEldol(tomb, szam) {
         eredmeny = [-1, false]
     }
     return eredmeny
+} */
+
+//másolás tétele
+
+/* let megtombosodesV3 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function add1(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        let eredetitombeleme = array[i];
+        newArray.push(eredetitombeleme + 1);
+    }
+    return newArray
+}
+
+ */
+
+
+
+let otosL = [5, 90, 6];
+let hatosL = [6, 45, 6];
+let skandiL = [7, 35, 6];
+let euroM = [5, 50, 10]
+
+function lottoHuzas(lotto) {
+    let nyertes = [];
+    let joker = []
+    for (let i = 0; i < lotto[0]; i++) {
+        nyertes.push(Math.floor(Math.random() * (lotto[1] - 1 + 1)))
+
+    }
+    if (lotto == euroM) {
+        joker.push(Math.floor(Math.random() * (lotto[2] - 1 + 1)))
+        joker.push(Math.floor(Math.random() * (lotto[2] - 1 + 1)))
+    } else {
+        joker.push(Math.floor(Math.random() * (lotto[2] - 1 + 1)))
+    }
+
+    return `a nyertes szamok ${nyertes} es a joker ${joker}`
+
 }
