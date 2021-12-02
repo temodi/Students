77314fa7ca528a2f018742128d0f4235e5fe76e7
szabolcs_derzsi <szabolcsnm@gmail.com>
2021-12-02 18:01:47 +0100

    lottogenerator javitas

--- Derzsi_Szabolcs/hazifeladat/dec-2.js
+++ Derzsi_Szabolcs/hazifeladat/dec-2.js
@@ -7,19 +7,27 @@ function random(max, min) {
 function lotto(szelveny) {
     let ujTomb = [];
     let ujTombHossza = 0;
+    let max = 0;
+    let min = 0;
 
     if (szelveny === 'otoslotto') {
         ujTombHossza = 5;
+        max = 90;
+        min = 1;
     } else if (szelveny === 'hatoslotto') {
         ujTombHossza = 6;
+        max = 45;
+        min = 1;
     } else if (szelveny === 'skandi') {
         ujTombHossza = 7;
+        max = 35;
+        min = 1;
     } else {
         return false;
     }
 
     for (let i = 0; i < ujTombHossza; i++) {
-        ujTomb.push(random(90, 1));
+        ujTomb.push(random(max, min));
     }
     return ujTomb;
 }
@@ -27,3 +35,4 @@ function lotto(szelveny) {
 console.log(lotto('otoslotto'));
 console.log(lotto('hatoslotto'));
 console.log(lotto('skandi'));
+console.log(lotto('asd'));
