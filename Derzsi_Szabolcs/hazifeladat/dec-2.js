c1e873bed63bed4d426e2e8ebd574d59d75b210a
szabolcs_derzsi <szabolcsnm@gmail.com>
2021-12-02 20:06:20 +0100

    kivalogatas callbackel

--- Derzsi_Szabolcs/hazifeladat/dec-2.js
+++ Derzsi_Szabolcs/hazifeladat/dec-2.js
@@ -36,3 +36,40 @@ console.log(lotto('otoslotto'));
 console.log(lotto('hatoslotto'));
 console.log(lotto('skandi'));
 console.log(lotto('asd'));
+
+
+// szamologep absztrakcioval
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a + b;
+}
+
+function szamologep(a, b, muvelet) {
+    return muvelet(a, b);
+}
+
+console.log(szamologep(1, 2, add));
+console.log(szamologep(1, 2, function (a, b) { return a / b}));
+
+
+// kivalogatas tetel
+
+function valogat(tomb, callback) {
+    let tomb2 = [];
+    for (let i = 0; i < tomb.length; i++) {
+        if (callback(tomb[i])) {
+            tomb2.push(tomb[i]);
+       }
+    }
+    return tomb2;
+}
+
+console.log(valogat([1,2,3,4,5,6], csakParos));
+
+function csakParos(szam) {
+    return szam % 2 === 0;
+}
